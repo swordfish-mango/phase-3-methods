d1ab4fa4cd67b7d6a9f67fcf52118c6b77496b24
Marcus <marcus.shimotsu@gmail.com>
2021-12-15 21:58:20 -0600

    commit tree 3973eccf0c9102f4f1cdad18e288d86661939bb5

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "progammer")
+    puts "Hello, #{name}!"
+end
+
+def add(a,b)
+    return a+b
+end
+
+def halve(a)
+    res = 0
+    if a.class != Integer 
+        res =  nil
+    else 
+        res = a/2
+return res
